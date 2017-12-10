a1b9c5c1fbc74ff429e0691e32d149524cc0182c
zhzhgang <zhang_zhonggang@163.com>
2017-12-10 23:02:17 +0800

    Eureka

---
+++ microservice-consumer-movie/src/main/java/com/zhzhgang/cloud/microservicesimpleconsumermovie/MicroserviceSimpleConsumerMovieApplication.java
@@ -0,0 +1,19 @@
+package com.zhzhgang.cloud.microservicesimpleconsumermovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class MicroserviceSimpleConsumerMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSimpleConsumerMovieApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
